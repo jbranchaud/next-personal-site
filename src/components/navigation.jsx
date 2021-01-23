66bbd6ee7a7020736e49879a3cada7f270a1ca0d
jbranchaud <jbranchaud@gmail.com>
2021-01-23 15:56:11 -0600

    Add temporary heading

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -295,7 +295,7 @@ const Navigation = () => {
       </nav>
       <header className="py-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-white">Dashboard</h1>
+          <h1 className="text-3xl font-bold text-white">Josh Branchaud</h1>
         </div>
       </header>
     </div>
